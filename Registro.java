b249a62fe09fd67f424dc264eaa3c68cd2deacfc
thiagograsso <143461239+thiagograsso@users.noreply.github.com>
2024-11-01 15:59:27 -0300

    Create Registro.java

---
+++ Registro.java
@@ -0,0 +1,11 @@
+public class Registro {
+    private String codigo;
+
+    public Registro(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+}
